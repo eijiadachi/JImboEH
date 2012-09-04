6d82025d4cc5a1afec06b5a5e47cefb12ca715be
Eiji Adachi <eijiadachi@gmail.com>
2012-09-04 17:38:35 -0300

    Versao Inicial

---
+++ JImboEH/src/jimboeh/popup/actions/SearchAction.java
@@ -0,0 +1,140 @@
+package jimboeh.popup.actions;
+
+import java.io.IOException;
+
+import jimboeh.Activator;
+import jimboeh.util.UtilAST;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.NodeFinder;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPartSite;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.statushandlers.StatusManager;
+
+public class SearchAction implements IEditorActionDelegate
+{
+
+	private IFile file;
+
+	private IEditorPart getActiveEditor()
+	{
+		final IWorkbench workbench = PlatformUI.getWorkbench();
+		final IWorkbenchWindow activeWorkbenchWindow = workbench.getActiveWorkbenchWindow();
+		final IWorkbenchPage activePage = activeWorkbenchWindow.getActivePage();
+
+		final IEditorPart activeEditor = activePage.getActiveEditor();
+
+		return activeEditor;
+	}
+
+	private IFile getCurrentFile(final IEditorPart targetEditor)
+	{
+		final IEditorInput editorInput = targetEditor.getEditorInput();
+
+		final IFileEditorInput fileEditorInput = (IFileEditorInput) editorInput;
+
+		final IFile file = fileEditorInput.getFile();
+
+		return file;
+	}
+
+	private MethodDeclaration getCurrentMethodDeclaration(final IFile file, final ITextSelection textSelection)
+			throws IOException, CoreException
+	{
+		final ASTNode rootNode = UtilAST.astNode( file );
+
+		final int offset = textSelection.getOffset();
+		final int length = textSelection.getLength();
+
+		final ASTNode currentNode = NodeFinder.perform( rootNode, offset, length );
+
+		final MethodDeclaration currentMethodDeclaration = UtilAST.getMethodDeclarationParent( currentNode );
+
+		if (currentMethodDeclaration == null)
+		{
+			final Status status = new Status( IStatus.WARNING, Activator.PLUGIN_ID,
+					"The cursor is not localized within a method declaration." );
+
+			throw new CoreException( status );
+		}
+
+		return currentMethodDeclaration;
+	}
+
+	private ITextSelection getCurrentTextSelection()
+	{
+		final IEditorPart activeEditor = getActiveEditor();
+
+		final ITextSelection textSelection = getCurrentTextSelection( activeEditor );
+
+		return textSelection;
+	}
+
+	private ITextSelection getCurrentTextSelection(final IEditorPart targetEditor)
+	{
+		final IWorkbenchPartSite site = targetEditor.getSite();
+		final ISelectionProvider selectionProvider = site.getSelectionProvider();
+		final ISelection selection = selectionProvider.getSelection();
+
+		final ITextSelection textSelection = (ITextSelection) selection;
+
+		return textSelection;
+	}
+
+	@Override
+	public void run(final IAction action)
+	{
+		try
+		{
+			final ITextSelection textSelection = getCurrentTextSelection();
+
+			final MethodDeclaration currentMethodDeclaration = getCurrentMethodDeclaration( file, textSelection );
+
+			MessageDialog.openInformation( null, "JImboEH",
+					"Search Action was executed." + currentMethodDeclaration.getName() );
+		}
+		catch (final IOException e)
+		{
+			final IStatus status = new Status( IStatus.ERROR, Activator.PLUGIN_ID, e.getMessage(), e );
+			final StatusManager statusManager = StatusManager.getManager();
+			statusManager.handle( status, StatusManager.SHOW | StatusManager.LOG );
+		}
+		catch (final CoreException e)
+		{
+			final IStatus status = e.getStatus();
+			final StatusManager statusManager = StatusManager.getManager();
+			statusManager.handle( status, StatusManager.SHOW | StatusManager.LOG );
+		}
+
+	}
+
+	@Override
+	public void selectionChanged(final IAction action, final ISelection selection)
+	{
+		// Do Nothing
+	}
+
+	@Override
+	public void setActiveEditor(final IAction action, final IEditorPart targetEditor)
+	{
+		file = getCurrentFile( targetEditor );
+	}
+
+}
---
+++ JImboEH/src/jimboeh/util/UtilAST.java
@@ -0,0 +1,72 @@
+package jimboeh.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+
+public class UtilAST
+{
+
+	public static ASTNode astNode(final IFile resource) throws IOException, CoreException
+	{
+		final InputStream fileContents = resource.getContents();
+
+		final BufferedReader reader = new BufferedReader( new InputStreamReader( fileContents ) );
+
+		final StringBuffer sb = new StringBuffer();
+
+		while (reader.ready())
+		{
+			sb.append( reader.readLine() );
+		}
+
+		final ICompilationUnit compilationUnit = JavaCore.createCompilationUnitFrom( resource );
+
+		final IProject project = resource.getProject();
+		final IJavaProject javaProject = JavaCore.create( project );
+
+		final ASTParser astParser = ASTParser.newParser( AST.JLS4 );
+		astParser.setSource( compilationUnit );
+		astParser.setKind( ASTParser.K_COMPILATION_UNIT );
+		astParser.setResolveBindings( true );
+		astParser.setBindingsRecovery( true );
+		astParser.setProject( javaProject );
+
+		final ASTNode rootNode = astParser.createAST( new NullProgressMonitor() );
+
+		return rootNode;
+	}
+
+	public static MethodDeclaration getMethodDeclarationParent(final ASTNode node)
+	{
+		ASTNode parent = node;
+
+		while (parent != null && !(parent instanceof MethodDeclaration))
+		{
+			parent = parent.getParent();
+		}
+
+		if (parent != null && parent instanceof MethodDeclaration)
+		{
+			return (MethodDeclaration) parent;
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+}
